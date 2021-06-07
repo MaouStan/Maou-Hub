6182ac7ef03845d6066993f8b63e22aa1d1d9b73
MaouStan <82369729+MaouStan@users.noreply.github.com>
2021-06-07 22:18:50 +0700

    Delete Main.lua

--- Main.lua
+++
@@ -1,3 +0,0 @@
-if (game.PlaceId == 6381829480 or game.PlaceId == 4520749081 or game.PlaceId == 5931540094) then
-    loadstring(game:HttpGet("https://maouhub.000webhostapp.com/MaouHubKingLegacy.lua"))()
-end
