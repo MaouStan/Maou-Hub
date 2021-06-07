615fd2aa0e59cccc03021e85eb2e9294bf148113
MaouStan <82369729+MaouStan@users.noreply.github.com>
2021-06-07 19:17:15 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,3 @@
 if (game.PlaceId == 6381829480 or game.PlaceId == 4520749081 or game.PlaceId == 5931540094) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MaouStan/Maou-Hub/main/King%20Legacy.lua"))()
+    loadstring(game:HttpGet("https://maouhub.000webhostapp.com/MaouHubKingLegacy.lua"))()
 end
